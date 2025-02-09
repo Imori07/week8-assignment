b125dba0f0ce5f9bbf1327358f1ceb4741f588ae
Yuvraj Sharma <galaxy0.07.04@gmail.com>
2025-02-09 17:27:21 +0000

    added delete button for posts

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,38 +1,48 @@
-// pages/posts/page.js
 import { db } from "@/utils/dbConnection";
 import Link from "next/link";
+import { revalidatePath } from "next/cache";
 
 export const metadata = {
   title: "Posts App - Posts",
   description: "A convenient list of all available posts in the app.",
 };
 
-export default async function PostsPage({ searchParams }) {
-  const { sort } = await searchParams;
+async function deletePost(formData) {
+  "use server";
 
+  const postId = formData.get("postId");
+  await db.query("DELETE FROM posts WHERE id = $1", [postId]);
+  revalidatePath("/posts");
+}
+
+export default async function PostsPage({ searchParams }) {
+  const { sort } = searchParams;
   const posts = await db.query("SELECT * FROM posts");
   const wrangledPosts = posts.rows;
 
-  // Sorting by title (A-Z and Z-A)
   if (sort === "desc") {
-    wrangledPosts.sort((a, b) => b.title.localeCompare(a.title)); // Sort by title in descending order
+    wrangledPosts.sort((a, b) => b.title.localeCompare(a.title));
   } else if (sort === "asc") {
-    wrangledPosts.sort((a, b) => a.title.localeCompare(b.title)); // Sort by title in ascending order
+    wrangledPosts.sort((a, b) => a.title.localeCompare(b.title));
   }
 
   return (
     <>
       <h1>A List of All Our Posts</h1>
-      <Link href={`/posts?sort=asc`}>A-Z </Link> |
-      <Link href={`/posts?sort=desc`}>Z-A</Link> |
-      <Link href={`/newPost`}>Create a New Post</Link> |
-      <Link href={`/`}>Home</Link>
+      <Link href="/posts?sort=asc">A-Z </Link> |
+      <Link href="/posts?sort=desc">Z-A</Link> |
+      <Link href="/newPost">Create a New Post</Link> |<Link href="/">Home</Link>
       <div>
         {wrangledPosts.map((post) => (
           <div key={post.id}>
             <h2>{post.title}</h2>
-            <Link href={`/posts/${post.id}`}>Read More</Link>{" "}
-            {/* Link to individual post */}
+            <Link href={`/posts/${post.id}`}>Read More</Link>
+
+            {/* Delete Button Form */}
+            <form action={deletePost}>
+              <input type="hidden" name="postId" value={post.id} />
+              <button type="submit">Delete</button>
+            </form>
           </div>
         ))}
       </div>
